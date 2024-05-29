c836e160155c33789c16639fe1673012f7712536
LorgioRoda <lorgiorodaroca@gmail.com>
2024-05-29 08:25:03 +0200

    test: :white_check_mark: pass first test

--- CHAPTER2-TDD/statsCalculator.ts
+++ CHAPTER2-TDD/statsCalculator.ts
@@ -1,2 +1,10 @@
 export const statsCalculator = (data) => {
+  let minNumber = data[0]
+  for (let index = 1; index < data.length; index++) {
+
+    if (data[index] < minNumber) {
+      minNumber = data[index];
+    }
+  }
+  return minNumber
 }
