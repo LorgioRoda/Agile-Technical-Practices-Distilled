e6b85de04b6123e6250dfe67219c89a3c57eb85d
LorgioRoda <lorgiorodaroca@gmail.com>
2024-05-31 12:27:01 +0200

    feat: :white_check_mark: add new tests

--- sectionTwoTDD/statsCalculator.test.ts
+++ sectionTwoTDD/statsCalculator.test.ts
@@ -1,10 +1,35 @@
-import { statsCalculator } from "./statsCalculator"
+import { statsCalculatorAverageValue, statsCalculatorMaxNumber, statsCalculatorMinNumber, statsCalculatorTotalSequence } from "./statsCalculator"
 
 const dataMock = [6,9,15,-2,92,11]
 
-describe('statsCalculator', () => { 
-  it('foo', () => {
-    const result = statsCalculator(dataMock)
+describe('statsCalculator', () => {
+  it('should return min number', () => {
+    const result = statsCalculatorMinNumber(dataMock)
     expect(result).toBe(-2)
   })
- })
+
+  it('should return max number', () => {
+    const result = statsCalculatorMaxNumber(dataMock)
+    expect(result).toBe(92)
+  })
+
+  describe('statsCalculatorTotalSequence', () => {
+    it('should return total of sequence', () => {
+      const result = statsCalculatorTotalSequence(dataMock)
+      expect(result).toBe(6)
+    })
+
+    it('should return 0 when dont has a list of elements', () => {
+      const elementMock = {}
+      const result = statsCalculatorTotalSequence(elementMock)
+      expect(result).toBe(0)
+    })
+  })
+
+  describe('statsCalculatorAverageValue', () => {
+    it('should return Averange Value', () => {
+      const result = statsCalculatorAverageValue(dataMock)
+      expect(result).toBe(21.833333333333332)
+    })
+  })
+})
--- sectionTwoTDD/statsCalculator.ts
+++ sectionTwoTDD/statsCalculator.ts
@@ -1,4 +1,4 @@
-export const statsCalculator = (data) => {
+export const statsCalculatorMinNumber = (data) => {
   let minNumber = data[0]
   for (let index = 1; index < data.length; index++) {
 
@@ -7,4 +7,26 @@ export const statsCalculator = (data) => {
     }
   }
   return minNumber
+}
+
+
+export const statsCalculatorMaxNumber = (data) => {
+  let number = data[0]
+  for (let index = 1; index < data.length; index++) {
+    if (data[index] > number) {
+      number = data[index];
+    }
+  }
+  return number
+}
+
+export const statsCalculatorTotalSequence = (data) => {
+  if(Array.isArray(data)) return data.length
+  return 0
+}
+
+export const statsCalculatorAverageValue = (data) => {
+  const totalOfElements = data.length
+  const total = data.reduce((acc, current) =>  acc + current ,0)
+  return total / totalOfElements
 }
